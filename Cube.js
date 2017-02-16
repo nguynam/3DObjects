7b057bfdaede98f886ff401c4e3f0443d8d389df
Joshua Crum <Josh@Joshuas-MacBook-Air.local>
2017-02-16 14:52:07 -0500

    Cube points and indices for top.

--- Cube.js
+++ Cube.js
@@ -6,12 +6,13 @@ class Cube {
         if (typeof col2 === "undefined") col2 = vec3.fromValues(Math.random(), Math.random(), Math.random());
         let randColor = vec3.create();
         let vertices = [];
-        let step = size / subDiv;
+        let step = size / (subDiv - 1);
         let top = [];
         let bottom = [];
         let frontBack = [];
         let rightLeft = [];
         this.indices = [];
+        this.topIndices = [];
         this.vbuff = gl.createBuffer();
 
         //top and bottom of cube
@@ -20,47 +21,31 @@ class Cube {
                 vec3.lerp(randColor, col1, col2, Math.random());
                 /* linear interpolation between two colors */
                 /* the next three floats are RGB */
-                if (col == 0) {
-                    top.push(row * step, col * step, size);
-                    top.push(randColor[0], randColor[1], randColor[2]);
-
-                    top.push((row + 1) * step, col * step, size);
-                    top.push(randColor[0], randColor[1], randColor[2]);
-
-                    top.push(row * step, (col + 1) * step, size);
-                    top.push(randColor[0], randColor[1], randColor[2]);
-                } else if (col == (subDiv - 1)) {
-                    top.push((row + 1) * step, col * step, size);
-                    top.push(randColor[0], randColor[1], randColor[2]);
-
-                    top.push((row + 1) * step, col * step, size);
-                    top.push(randColor[0], randColor[1], randColor[2]);
-
-                    top.push((row + 1) * step, (col - (subDiv - 1)) * step, size);
-                    top.push(randColor[0], randColor[1], randColor[2]);
-
-                } else {
-                    top.push((row + 1)* step, col * step, size);
-                    top.push(randColor[0], randColor[1], randColor[2]);
-
-                    top.push(row * step, (col + 1) * step, size);
-                    top.push(randColor[0], randColor[1], randColor[2]);
-
-                }
-
+                top.push(row * step, col * step, size);
+                top.push(randColor[0], randColor[1], randColor[2]);
             }
         }
+
+        //right and left side of cube
+
+        //front and back side of cube
 
         this.topBuff = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, this.topBuff);
         gl.bufferData(gl.ARRAY_BUFFER, Uint16Array.from(top), gl.STATIC_DRAW);
 
+        //generate the order of the points
+        for (let k = 0; k < subDiv; k++) {
+            for (let l = 0; l < subDiv; l++) {
+                if (k < subDiv - 1) {
+                    this.topIndices.push(subDiv * k + l);
+                    this.topIndices.push(subDiv * (k + 1) + l);
+                }
+            }
+        }
+
         var x = {"primitive": gl.POINTS, "buffer": this.topBuff, "numPoints": top.length};
         this.indices.push(x);
-
-        for (let k = 0; k < this.indices.size; k++) {
-
-        }
 
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.vbuff);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, Float32Array.from(this.indices), gl.STATIC_DRAW);
