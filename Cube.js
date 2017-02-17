09a159d400e7973a187e5884873fc8f196eb2e1d
Joshua Crum <Josh@Joshuas-MacBook-Air.local>
2017-02-17 00:43:53 -0500

    Cube logic working, points need some work. The top and bottom faces of the cube face the same direction.

--- Cube.js
+++ Cube.js
@@ -5,25 +5,29 @@ class Cube {
         if (typeof col1 === "undefined") col1 = vec3.fromValues(Math.random(), Math.random(), Math.random());
         if (typeof col2 === "undefined") col2 = vec3.fromValues(Math.random(), Math.random(), Math.random());
         let randColor = vec3.create();
+        this.indices = [];
+        this.vbuff = gl.createBuffer();
         let step = size / (subDiv - 1);
+        let vertices = [];
 
         //top face of the cube
         let top = [];
-        this.topIndexBuff;
-        this.topBuff = gl.createBuffer();
         this.topIndices = [];
 
         //bottom face of the cube
         let bottom = [];
-        this.bottomIndexBuff;
-        this.bottomBuff = gl.createBuffer();
         this.bottomIndices = [];
 
-        this.indices = [];
+        //front face of the cube
+        let front = [];
+        this.frontIndices = [];
 
-        let x = -size / 2;
+        //back of the cube
+        let back = [];
+        this.backIndices = [];
 
         //top and bottom of cube
+        let x = -size / 2;
         for (let row = 0; row < subDiv; row++) {
             let y = -size / 2;
             for (let col = 0; col < subDiv; col++) {
@@ -38,42 +42,89 @@ class Cube {
             x += step;
         }
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.topBuff);
-        gl.bufferData(gl.ARRAY_BUFFER, Float32Array.from(top), gl.STATIC_DRAW);
+        //front and back side of cube
+        let xx = -size / 2;
+        for (let row = 0; row < subDiv; row++) {
+            let yy = -size / 2;
+            for (let col = 0; col < subDiv; col++) {
+                vec3.lerp(randColor, col1, col2, Math.random());
+                front.push(size / 2, yy, xx);
+                front.push(randColor[0], randColor[1], randColor[2]);
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.bottomBuff);
-        gl.bufferData(gl.ARRAY_BUFFER, Float32Array.from(bottom), gl.STATIC_DRAW);
+                back.push(-size / 2, xx, yy);
+                back.push(randColor[0], randColor[1], randColor[2]);
+                yy += step;
+            }
+            x += step;
+        }
 
         //right and left side of cube
 
-        //front and back side of cube
-
         //generate the order of the points
+
+        let facePoints = subDiv * subDiv;
         for (let k = 0; k < subDiv; k++) {
             for (let l = 0; l < subDiv; l++) {
                 if (k < subDiv - 1) {
                     this.topIndices.push(subDiv * k + l);
                     this.topIndices.push(subDiv * (k + 1) + l);
 
-                    this.bottomIndices.push(subDiv * k + l);
-                    this.bottomIndices.push(subDiv * (k + 1) + l);
+                    this.bottomIndices.push((subDiv * k + l) + facePoints);
+                    this.bottomIndices.push((subDiv * (k + 1) + l) + facePoints);
+
+                    this.frontIndices.push((subDiv * k + l) + (facePoints * 2));
+                    this.frontIndices.push((subDiv * (k + 1) + l) + (facePoints * 2));
+
+                    this.backIndices.push((subDiv * k + l) + (facePoints * 3));
+                    this.backIndices.push((subDiv * (k + 1) + l) + (facePoints * 3));
                 }
             }
         }
 
-        this.topIndexBuff = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.topIndexBuff);
+        for (let topI = 0; topI < top.length; topI++) {
+            vertices.push(top[topI]);
+        }
+
+        for (let botI = 0; botI < bottom.length; botI++) {
+            vertices.push(bottom[botI]);
+        }
+
+        for (let frontI = 0; frontI < bottom.length; frontI++) {
+            vertices.push(front[frontI]);
+        }
+
+        for (let backI = 0; backI < bottom.length; backI++) {
+            vertices.push(back[backI]);
+        }
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vbuff);
+        gl.bufferData(gl.ARRAY_BUFFER, Float32Array.from(vertices), gl.STATIC_DRAW);
+
+        this.topIdxBuff = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.topIdxBuff);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, Uint16Array.from(this.topIndices), gl.STATIC_DRAW);
 
-        let topFace = {"primitive": gl.TRIANGLE_STRIP, "buffer": this.topIndexBuff, "numPoints": this.topIndices.length};
-        this.indices.push(topFace);
-
-        this.bottomIndexBuff = gl.createBuffer();
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.bottomIndexBuff);
+        this.botIdxBuff = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.botIdxBuff);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, Uint16Array.from(this.bottomIndices), gl.STATIC_DRAW);
 
-        let bottomFace = {"primitive": gl.TRIANGLE_STRIP, "buffer": this.bottomIndexBuff, "numPoints": this.bottomIndices.length};
+        this.frontIdxBuff = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.frontIdxBuff);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, Uint16Array.from(this.frontIndices), gl.STATIC_DRAW);
+
+        this.backIdxBuff = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.backIdxBuff);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, Uint16Array.from(this.backIndices), gl.STATIC_DRAW);
+
+        let topFace = {"primitive": gl.TRIANGLE_STRIP, "buffer": this.topIdxBuff, "numPoints": this.topIndices.length};
+        let bottomFace = {"primitive": gl.TRIANGLE_STRIP, "buffer": this.botIdxBuff, "numPoints": this.bottomIndices.length};
+        let frontFace = {"primitive": gl.POINTS, "buffer": this.frontIdxBuff, "numPoints": this.frontIndices.length};
+        let backFace = {"primitive": gl.POINTS, "buffer": this.backIdxBuff, "numPoints": this.backIndices.length};
+
+        this.indices.push(topFace);
         this.indices.push(bottomFace);
+        this.indices.push(frontFace);
+        this.indices.push(backFace);
     }
 
     draw(vertexAttr, colorAttr, modelUniform, coordFrame) {
@@ -81,7 +132,7 @@ class Cube {
         gl.uniformMatrix4fv(modelUniform, false, coordFrame);
 
         /* binder the (vertex+color) buffer */
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.topIndexBuff);
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vbuff);
 
         /* with the "packed layout"  (x,y,z,r,g,b),
          the stride distance between one group to the next is 24 bytes */
@@ -96,4 +147,5 @@ class Cube {
             gl.drawElements(obj.primitive, obj.numPoints, gl.UNSIGNED_SHORT, 0);
         }
     }
+
 }
